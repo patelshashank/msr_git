0493f539791aed54f98b15309935a38eb6299461
patelshashank <patelshashank@gmail.com>
2019-08-20 22:04:25 -0500

    Update DeployPricingMSRToK8s.sh

--- DeployPricingMSRToK8s.sh
+++ DeployPricingMSRToK8s.sh
@@ -3,4 +3,6 @@
 
 cd /opt/rancher
 ./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+echo "login"
 ./rancher kubectl create -f ${WORKSPACE}/PushPricingmsrToK8S.yaml
+echo "pushpricingmsr"
